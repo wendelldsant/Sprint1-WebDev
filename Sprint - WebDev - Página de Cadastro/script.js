dc7269adcb3432a01e951759a38c8962296f68cb
wendelldsant <wendell_dsantos@hotmail.com>
2024-05-26 21:59:15 -0300

    feat-error-message-id

--- Sprint - WebDev - Página de Cadastro/script.js
+++ Sprint - WebDev - Página de Cadastro/script.js
@@ -1,56 +1,33 @@
-// function verifica_ID(event) {
-//     event.preventDefault();
+const username_id = document.getElementById('username');
+const btn_cadastrar = document.getElementById('btn_cadastrar');
+const error_msg_username = document.getElementById('error-message')
+const name_id = document.getElementById('name')
 
-//     let id = document.getElementById('username').value;
-//     let msgErro = document.getElementById('error-message');
-//     let formulario = document.getElementById('registration-form')
-//     formulario.setAttribute('style', 'background-color: red');
-//     if (id === '2') { 
-//      msgErro.innerHTML = 'Nome de Usuário Inválido';
-//     }   
-
-// }
-
-
-// document.getElementById('registration-form').addEventListener('submit', verifica_ID);
-
-// function verifica_ID_senha(event) {
-//     event.preventDefault();
-
-//     let id = document.getElementById('senha').value;
-//     let msgErro = document.getElementById('error-message');
-
-//     if (id === '2' && senha === '3') { 
-//         msgErro.innerHTML = 'Nome de Usuário e Senha Inválido';
-//     }   
-// }
-
-let cadastrar_btn = document.getElementById('Cadastrar')
-
-function verificaUsername(id,event){
-    let campo_id = document.querySelector(id)
-    let usuario_id = document.getElementById('username').value;
-    let erro_msg_username = document.getElementById('error-message-username');
-
-    event.preventDefault();
-    if(usuario_id.length<7){
-        erro_msg_username.innerHTML = '<p>Usuário deve conter mais de 7 digitos</p>';
+function verificaEspacos(inputValue){
+    if(inputValue.includes(' ')){
+        console.log('alert')
+        return true
     }
     else{
-        erro_msg_username.innerHTML = '';
-        console.log('usuario aprovado');  
-        campo_id.setAttribute('style', 'border-color: green; border-width: 2px');   
+        return false
     }
-
+}
+function verificaUsername(){
+    let username = username_id.value;
+    if(username.length<10 || verificaEspacos(username)){
+        console.log('okokok')
+        error_msg_username.innerHTML = 'O ID de usuário deve ter no mínimo 10 caracteres'
+        error_msg_username.setAttribute('style', 'color: red')
+    }
+    else{
+        error_msg_username.innerHTML = ''
+        username_id.setAttribute('style', 'border-color: green; border-width: 2px')
+    }
 }
 
-// function verificaNome(id){
-//     let usuario_nome = document.getElementById("name");
-//     if (isNaN(usuario_nome)){
-//         erro_msg_name.innerHTML = '';
-//         campo_id.setAttribute('style', 'border-color: green; border-width: 2px');
-//     }
-// }
 
-cadastrar_btn.addEventListener('click', verificaUsername())
+btn_cadastrar.addEventListener('click', function(event){
+    event.preventDefault();
+    verificaUsername()
+})
 
