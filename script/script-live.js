9a4f27862f69339a3852eea76656a7be4ef757fd
wendelldsant <wendell_dsantos@hotmail.com>
2024-06-11 21:35:12 -0300

    fix - Bugs no Chat

--- script/script-live.js
+++ script/script-live.js
@@ -1,15 +1,18 @@
+const listaUser = JSON.parse(localStorage.getItem('users'));
+const loginCheck = JSON.parse(localStorage.getItem('loginCheck'));
 userTeste = 'wendelldsant';
 listaLiveMessage = [];
-
+const campoMessages = document.querySelector('#chat-messages');
+const btnEnviar = document.querySelector('#chat-send');
 // #CREATE
 
 function criaMensagem(dados){
     const newMessage = {
         message: dados.message,
         username: dados.username,
-        idMessage: listaLiveMessage.lenght+1
+        idMessage: listaLiveMessage.length+1
     }
-    listaLiveMessage.push(newMessage)
+    listaLiveMessage.push(newMessage);
     readMessages();
 }
 
@@ -17,26 +20,28 @@ function criaMensagem(dados){
 // #READ
 
 function readMessages(){
-    const campoMessages = document.querySelector('#chat-messages');
     campoMessages.innerHTML = '';
-    listaLiveMessage.forEach(element => {
-        let message = document.createElement('div');
-        message.id = `message${element.idMessage}`
-        message.innerHTML = `
-            <p> ${element.username} ${element.message}</p>
-        `
-        campoMessages.append(element)
-    });
+    if (listaLiveMessage.length!=0){
+        listaLiveMessage.forEach(element => {
+            const message = document.createElement('div');
+            message.id = `message${element.idMessage}`
+            message.innerHTML = `
+                <p> ${element.username}: ${element.message}</p>
+            `
+            campoMessages.append(message)
+        });
+    }
+    else{
+        console.log('null')
+    }
+
 }
 
-const btnEnviar = document.querySelector('#chat-send');
+
 btnEnviar.addEventListener('click', function(event){
     event.preventDefault();
     let newMessage = criaMensagem({
         message: document.getElementById("chat-input").value,
-        username: userTeste,
-        id: 1
+        username: userTeste
     });
-
-    readMessages();
 })
